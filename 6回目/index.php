b3d02c172bd6b919a9ac17bb99fb31f6e3b00727
RaaanaD <83898574+RaaanaD@users.noreply.github.com>
2021-06-25 21:22:13 +0900

    Update index.php

--- 6回目/index.php
+++ 6回目/index.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>データ登録</title>
+  <title>ポン太カフェ感想登録</title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <style>div{padding: 10px;font-size:16px;}</style>
 </head>
